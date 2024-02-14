10b29e378484e6d79aafe42f423c1142c7196b48
DavidDevGt <josuedavidvl18@gmail.com>
2024-02-13 23:12:56 -0600

    Añadir métodos CRUD para Notebook y User

--- src/Model/Notebook.js
+++ src/Model/Notebook.js
@@ -1,9 +1,56 @@
 class Notebook {
     constructor(db) {
-        this.db = db;
+      this.db = db;
     }
-
-    // CRUD Notebook
-}
-
-module.exports = Notebook;
+  
+    // Create
+    create(name, user_id, callback) {
+      const sql = "INSERT INTO notebooks (name, user_id) VALUES (?, ?)";
+      this.db.run(sql, [name, user_id], function(err) {
+        callback(err, { id: this.lastID });
+      });
+    }
+  
+    // Read
+    findAll(callback) {
+      const sql = "SELECT * FROM notebooks";
+      this.db.all(sql, [], (err, rows) => {
+        callback(err, rows);
+      });
+    }
+  
+    findById(id, callback) {
+      const sql = "SELECT * FROM notebooks WHERE id = ?";
+      this.db.get(sql, [id], (err, row) => {
+        callback(err, row);
+      });
+    }
+  
+    // Update
+    update(id, name, callback) {
+      const sql = "UPDATE notebooks SET name = ? WHERE id = ?";
+      this.db.run(sql, [name, id], function(err) {
+        callback(err, { changes: this.changes });
+      });
+    }
+  
+    // Delete
+    delete(id, callback) {
+        // Primero eliminamos todas las notas asociadas al notebook
+        const deleteNotesSql = "DELETE FROM notes WHERE notebook_id = ?";
+        this.db.run(deleteNotesSql, [id], (err, row) => {
+          if (err) {
+            callback(err);
+            return;
+          }
+          // Después eliminamos el notebook
+          const deleteNotebookSql = "DELETE FROM notebooks WHERE id = ?";
+          this.db.run(deleteNotebookSql, [id], function(err) {
+            callback(err, { changes: this.changes });
+          });
+        });
+      }
+  }
+  
+  module.exports = Notebook;
+  
--- src/Model/User.js
+++ src/Model/User.js
@@ -1,9 +1,59 @@
 class User {
     constructor(db) {
-        this.db = db;
+      this.db = db;
     }
-
-    // CRUD user
-}
-
-module.exports = User;
+  
+    create(username, callback) {
+      const sql = "INSERT INTO users (username) VALUES (?)";
+      this.db.run(sql, [username], function(err) {
+        callback(err, { id: this.lastID });
+      });
+    }
+  
+    findAll(callback) {
+      const sql = "SELECT * FROM users";
+      this.db.all(sql, [], (err, rows) => {
+        callback(err, rows);
+      });
+    }
+  
+    findById(id, callback) {
+      const sql = "SELECT * FROM users WHERE id = ?";
+      this.db.get(sql, [id], (err, row) => {
+        callback(err, row);
+      });
+    }
+  
+    update(id, username, callback) {
+      const sql = "UPDATE users SET username = ? WHERE id = ?";
+      this.db.run(sql, [username, id], function(err) {
+        callback(err, { changes: this.changes });
+      });
+    }
+  
+    delete(id, callback) {
+      const sql = "DELETE FROM users WHERE id = ?";
+      this.db.run(sql, [id], function(err) {
+        callback(err, { changes: this.changes });
+      });
+    }
+  
+    // Método para verificar la existencia de un usuario o crear uno nuevo
+    findOrCreate(username, callback) {
+      const findSql = "SELECT * FROM users WHERE username = ?";
+      this.db.get(findSql, [username], (err, row) => {
+        if (err) {
+          callback(err);
+          return;
+        }
+        if (row) {
+          callback(null, row);
+        } else {
+          this.create(username, callback);
+        }
+      });
+    }
+  }
+  
+  module.exports = User;
+  
